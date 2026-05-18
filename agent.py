aaa7f4df3ffb418cab29bee09128784cab66ef74
Ryan Zhang <ryzhangofficial@gmail.com>
2026-05-18 17:37:30 -0400

    feat: add random agent

--- agent.py
+++ agent.py
@@ -14,3 +14,19 @@ class UniformAgent(Agent):
         
     def act(self, history):
         return [20,20,20,20,20]
+
+class RandomAgent(Agent):
+    def __init__(self, num_battlefields=5, total_resources=100):
+        super().__init__("RandomAgent")
+        self.num_battlefields = num_battlefields
+        self.total_resources = total_resources
+        
+    def act(self, history):
+        cuts = sorted(random.sample(range(self.total_resources+1), self.num_battlefields-1))
+        values = [cuts[0]]
+        
+        for i in range(1, len(cuts)):
+            values.append(cuts[i] - cuts[i-1])
+            
+        values.append(self.total_resources - cuts[-1])
+        return values
