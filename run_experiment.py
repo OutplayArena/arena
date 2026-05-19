fc1c071c8149a505eeca01c785f879acb50c7ca4
Ryan Zhang <ryzhangofficial@gmail.com>
2026-05-19 11:37:04 -0400

    feat: update experiment runner

--- run_experiment.py
+++ run_experiment.py
@@ -1,11 +1,30 @@
+import argparse
+
 from api import BlottoGame
-from agent import Agent, UniformAgent, RandomAgent
+from agent import Agent, UniformAgent, RandomAgent, GreedyAgent, LLMAgent
+
+AGENTS = {
+    "uniform": UniformAgent,
+    "random": RandomAgent,
+    "greedy": GreedyAgent,
+    "llm": LLMAgent
+}
 
 def run_experiment(agent_a:Agent, agent_b:Agent, num_rounds=10):
     blotto_game = BlottoGame(num_battlefields=5, total_resources=100)
     
     result = blotto_game.play_match(agent_a, agent_b, num_rounds)
-    print(f"Winner of match is {result["match_winner"]}")
+    print(f'Winner of match is {result["match_winner"]}')
 
 if __name__ == "__main__":
-    run_experiment(RandomAgent(), RandomAgent(), 99)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--agent_a", type=str, default="uniform")
+    parser.add_argument("--agent_b", type=str, default="random")
+    parser.add_argument("--rounds", type=int, default=10)
+    
+    args = parser.parse_args()
+    agent_a = AGENTS[args.agent_a]()
+    agent_b = AGENTS[args.agent_b]()
+    rounds = args.rounds
+    
+    run_experiment(agent_a, agent_b, rounds)
